7c845a43ea9d48074da38b03bc64bcc748b3c28e
Krystian Otto <otto.krystiann@gmail.com>
2020-06-11 14:57:52 +0200

    add fully functional blog scripts

--- js/script.js
+++ js/script.js
@@ -1,29 +1,35 @@
 'use strict';
 
 const titleClickHandler = function(event){
-    console.log('Link was clicked!');
+    event.preventDefault();
+    const clickedElement = this;
   
-    /* remove class 'active' from all article links  */
+    /*  [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
+    /* [DONE] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
     
     for (let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const tartgetArticle = document.querySelector(articleSelector);
+    console.log(tartgetArticle)
+
+    /* [DONE] add class 'active' to the correct article */
+    tartgetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
