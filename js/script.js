d48032176ad498e34f5802cbd6c1288a5dab89cf
Krystian Otto <otto.krystiann@gmail.com>
2020-06-13 11:38:53 +0200

    add new variables and consts to improve readability of code

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ const titleClickHandler = function(event){
   
     /*  [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
-
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
@@ -16,7 +15,6 @@ const titleClickHandler = function(event){
 
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
-    
     for (let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
@@ -32,28 +30,34 @@ const titleClickHandler = function(event){
     tartgetArticle.classList.add('active');
 }
 
-
 const articles = document.querySelectorAll('.post');
-  
+const optTitleListSelector = '.titles';
+const optTitleSelector = '.post-title';
+const articeTitleList = document.querySelector(optTitleListSelector);
 
+let html = '';
 
 function generateTitleLinks(){
-    //Clean siddebar
-    document.querySelector('.titles').innerHTML = '';
+    //clean siddebar
+    articeTitleList.innerHTML = '';
     //get article id and title
     for(let article of articles){
         const articleID = article.getAttribute('id');
-        const articleTitle = article.querySelector('h3').innerHTML;
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
         
-        document.querySelector('.titles').innerHTML += ("<li><a href='#"+articleID+"'><span>"+articleTitle+"</span></a></li>");
-        console.log(document.querySelector('.titles'))
+        //insert into html updated list
+        const linkHTML = "<li><a href='#"+articleID+"'><span>"+articleTitle+"</span></a></li>";
+        html += linkHTML;
     }
+
+    // insert into html code updated list
+    articeTitleList.innerHTML += html;
+
+    //add event which open corect article on page
     const links = document.querySelectorAll('.titles a');
     for(let link of links){
         link.addEventListener('click', titleClickHandler);
     }
-    //save id and title to html 
-    //paste id and title into sidebar
 }
 
 generateTitleLinks()
